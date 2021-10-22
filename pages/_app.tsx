c705686497a35b1dd39bb6c3139c0128e39b74d6
david-snyder-induro <david.snyder@induro.io>
2021-10-21 22:32:36 -0400

    setting up styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,22 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return (
+        <>
+            <Head>
+                <link
+                    rel="preload"
+                    href="https://cdn.jsdelivr.net/gh//GypsyDangerous/simple-css-reset/reset.css"
+                    as="style"
+                />
+                <link
+                    rel="stylesheet"
+                    href="https://cdn.jsdelivr.net/gh//GypsyDangerous/simple-css-reset/reset.css"
+                />
+            </Head>
+            <Component {...pageProps} />
+        </>
+    );
 }
-export default MyApp
+export default MyApp;
